8135ab39c8c92e85b4d501eefc5dd65ae0d43b0c
Jose <josesquiveluis@gmail.com>
2019-03-14 16:36:08 -0600

    The excercise 1 is completed for the example case.
    The program prints the number of mines found in the input file.

--- openingTxt/readTXT.cpp
+++ openingTxt/readTXT.cpp
@@ -46,7 +46,8 @@ int main(int argc, char* argv[]) {
 
     //cout<<"Rows "<<Rows<<" "<<"Cols "<<Cols<<endl;
 
-    int MAP[Rows][Cols];
+    int MAP[Rows][Cols]; //array to save the map
+    int Mines[Rows+2][Cols+2]; //array to count the number of mines
 
     for (int i = 0; i < Rows; i++){     //iterate to get each value in the input assigned to a position inside the int array MAP
         getline(myReadFile, sLine); //saving the current row to the variable sLine
@@ -56,6 +57,68 @@ int main(int argc, char* argv[]) {
         }
         //cout<<" "<<endl;
     }
+
+    for(int i = 0; i < Rows; i++){          //copy the MAP array into the Mines array but with a border
+        for(int j = 0; j < Cols; j++){
+            Mines[i+1][j+1] = MAP[i][j];
+        }
+    }
+
+    for(int i = 0; i < Rows +2; i++){       //fill in the border of array Mine with number 2
+        Mines[i][0] = 2;
+        Mines[i][Rows+1] = 2;
+    }
+
+    for(int i = 0; i < Cols +2; i++){      //fill in the border of array Mine with number 2
+        Mines[0][i] = 2;
+        Mines[Cols+1][i] = 2;
+    }    
+
+    /*for(int i = 0; i < Rows+2; i++){
+        for(int j = 0; j < Cols+2; j++){
+            cout<<Mines[i][j];
+        }
+        cout<<" "<<endl;
+    }*/
+
+    int currentMinesNumber = -1;
+
+    int Operations[8][2] = {{-1,-1},{0,-1},{1,-1},{-1,0},{1,0},{-1,1},{0,1},{1,1}};
+
+    for(int i = 0; i < Rows; i++){
+        for(int j = 0; j < Cols; j++){
+            if(Mines[i+1][j+1] == 1 && currentMinesNumber == -1){
+                Mines[i+1][j+1] = currentMinesNumber;
+                currentMinesNumber--;
+            }
+            else if(Mines[i+1][j+1] == 1){
+
+                for (int k =0; k < 8; k++){
+                    if(Mines[i+1+Operations[k][0]][j+1+Operations[k][1]] < 0){
+                        Mines[i+1][j+1] = Mines[i+1+Operations[k][0]][j+1+Operations[k][1]];
+                        break;
+                    }
+                    if(k == 7){
+                        Mines[i+1][j+1] = currentMinesNumber;
+                        currentMinesNumber--;
+                    }                        
+                }                
+                    
+
+            }
+        }
+    }
+
+    for(int i = 0; i < Rows+2; i++){
+        for(int j = 0; j < Cols+2; j++){
+            cout<<Mines[i][j];
+        }
+        cout<<" "<<endl;
+    }    
+
+    cout<<-(currentMinesNumber+1)<<endl;    
+
+
 
   }
 
