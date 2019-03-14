1ada63885028e339db9dc6270b848e9b521cbdac
Jose <josesquiveluis@gmail.com>
2019-03-14 15:09:28 -0600

    Reading of input file and assignment to an array.
    The program is capable of reading the input file and assign it to an integer array.

--- openingTxt/cmake_install.cmake
+++ openingTxt/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/jose/Documents/skycatch/openingTxt
+# Install script for directory: /home/jose/Documents/SkCatch/SKcatch/openingTxt
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/jose/Documents/skycatch/openingTxt/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/jose/Documents/SkCatch/SKcatch/openingTxt/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- openingTxt/readTXT.cpp
+++ openingTxt/readTXT.cpp
@@ -1,32 +1,65 @@
 #include <iostream>
 #include <fstream>
+#include <typeinfo>
+#include <string> 
 using namespace std;
 
 int main(int argc, char* argv[]) {
- //string InputTXT = argc > 1 ? argv[1] : "there was no input";
+ 
  char* name = argv[1];
  if(argc>1){
- 	cout<<"starting execution"<<endl;
+    cout<<"starting execution"<<endl;
  }
  else{
- 	cout<<"no input file, execution finished"<<endl;
- 	return -1;
+    cout<<"no input file, execution finished"<<endl;
+    return -1;
  }
 
  ifstream myReadFile;
- //char* name = "input.txt";
+ 
  myReadFile.open(name);
  char output[100];
- if (myReadFile.is_open()) {
- while (!myReadFile.eof()) {
+ string nRows;
+ string nCols;
+
+  if (myReadFile.good()) //reading the status of the file
+  {
+    string sLine;        //variable to store the current row from the input file
+    getline(myReadFile, sLine); //saving the current row to the variable sLine
+    //cout << sLine.length() << endl; //getting the number of characters inside sLine
+    int spaceIndx;          
+    for(int i = 0; i < sLine.length(); i++){ //iterate inside the first row
+        //cout << sLine[i];
+        if (sLine[i] == ' '){       //in case a space char is found, its position is saved in spaceIndx
+            spaceIndx = i;
+        }
+    }
+    //cout<<"indice"<<spaceIndx<<endl;
+
+    nRows = sLine.substr (0,spaceIndx); //substring to get the number of rows as a String
+    //cout<<"nRows "<<nRows<<endl;
+    nCols = sLine.substr (spaceIndx+1,sLine.length()); //substring to get the number of columns as a String
+    //cout<<"nCols "<<nCols<<endl;
+
+    int Rows = stoi(nRows);  //conversion from string to int
+    int Cols = stoi(nCols);
+
+    //cout<<"Rows "<<Rows<<" "<<"Cols "<<Cols<<endl;
+
+    int MAP[Rows][Cols];
+
+    for (int i = 0; i < Rows; i++){     //iterate to get each value in the input assigned to a position inside the int array MAP
+        getline(myReadFile, sLine); //saving the current row to the variable sLine
+        for (int j = 0; j < Cols; j++){
+            //cout << sLine[j];   
+            MAP[i][j] =  sLine[j]- 48; //conversion to int, since the only values are 0 and 1, it is possible to do this.
+        }
+        //cout<<" "<<endl;
+    }
+
+  }
 
 
-    myReadFile >> output;
-    cout<<output;
-
-
- }
-}
 myReadFile.close();
 return 0;
 }
